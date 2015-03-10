921c1e9e0a5c79783505fcf1506e254f315c5a88
Adam Kowalczyk <adamkowalczyk@gmail.com>
2015-03-10 17:17:35 +0000

    integrating jade

--- views.js
+++ views.js
@@ -1,15 +1,27 @@
-// TODO use jade 
+var jade = require('jade');
+var path = require('path');
 
-exports.edit = 
-'<h1>Create new post</h1>'+
-'<form action="edit" method="POST">'+
-	'Name:'+
-	'<input type="text" name="author"></input>'+
-	'Post Title:'+
-	'<input type="text" name="title"></input>'+
-	'Text:'+
-	'<input type="text" name="text"></input>'+
-	'<input type="submit" name="Submit"></input>'+
-'</form>';
+var files = {
+	edit : path.join(__dirname,'/templates/edit.jade')
+};
+
+module.exports = {
+	edit : jade.renderFile(files.edit, {test: 'OH HAI THERE'})
+};
+
+
+
+
+// exports.edit = 
+// '<h1>Create new post</h1>'+
+// '<form action="edit" method="POST">'+
+// 	'Name:'+
+// 	'<input type="text" name="author"></input>'+
+// 	'Post Title:'+
+// 	'<input type="text" name="title"></input>'+
+// 	'Text:'+
+// 	'<input type="text" name="text"></input>'+
+// 	'<input type="submit" name="Submit"></input>'+
+// '</form>';
 
 
